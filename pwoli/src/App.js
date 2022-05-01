4bc35ecf36882b4cdcc405444b3edd655c2d32d6
Joel Rajesh K <joelrajeshk@Joels-MBP.bbrouter>
2022-05-01 11:24:11 +0530

    Added initial code and categories basic component

---
+++ pwoli/src/App.js
@@ -0,0 +1,42 @@
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneakers",
+    },
+    {
+      id: 1,
+      title: "Womens",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <div className="category-container">
+            <div className="background-image"></div>
+            <div className="category-body-container">
+              <h2>{category.title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
